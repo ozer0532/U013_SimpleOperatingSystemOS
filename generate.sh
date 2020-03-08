a4bc684ca780875ae81adb38c0e028c1e9bd475f
lionnarta <13518128@std.stei.itb.ac.id>
2020-03-08 18:46:02 +0700

    Refactoring folder structure

--- generate.sh
+++ generate.sh
@@ -7,8 +7,11 @@ nasm ./ass/bootloader.asm -o ./img/bootloader.img
 # Insert bootloader to sector 0
 dd if=./img/bootloader.img of=system.img bs=512 count=1 conv=notrunc
 
-# Insert map to sector 1
-dd if=./img/map.img of=system.img bs=512 count=1 seek=1 conv=notrunc
+# Insert map to sector 0x100
+dd if=./img/map.img of=system.img bs=512 count=1 seek=0x100 conv=notrunc
 
-# Insert dir to sector 2
-dd if=./img/dir.img of=system.img bs=512 count=1 seek=2 conv=notrunc
+# Insert files to sector 0x101
+dd if=./img/files.img of=system.img bs=1024 count=1 seek=0x101 conv=notrunc
+
+# Insert sectors to sector 0x103
+dd if=./img/sectors.img of=system.img bs=512 count=1 seek=0x103 conv=notrunc
