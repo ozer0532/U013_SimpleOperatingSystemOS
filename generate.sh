a291dcb202b9c25c8f5ddcba83f7dbfcf49a03ec
mkamadeus <mkamadeus.mka@gmail.com>
2020-03-08 18:04:34 +0700

    Added milestone 2 file, removed current redundant files

--- generate.sh
+++ generate.sh
@@ -7,8 +7,11 @@ nasm ./ass/bootloader.asm -o ./img/bootloader.img
 # Insert bootloader to sector 0
 dd if=./img/bootloader.img of=system.img bs=512 count=1 conv=notrunc
 
-# Insert map to sector 1
-dd if=./img/map.img of=system.img bs=512 count=1 seek=1 conv=notrunc
+# Insert map to sector 0x100 (256)
+dd if=./img/map.img of=system.img bs=512 count=1 seek=256 conv=notrunc
 
-# Insert dir to sector 2
-dd if=./img/dir.img of=system.img bs=512 count=1 seek=2 conv=notrunc
+# Insert files to sector 0x101 and 0x102 (257, 258)
+dd if=./img/files.img of=system.img bs=1024 count=1 seek=257 conv=notrunc
+
+# Insert sectors to sector 0x103 (259)
+dd if=./img/sectors.img of=system.img bs=1024 count=1 seek=259 conv=notrunc
